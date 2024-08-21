2b8dc25070b75b0704b9db27f9dc0e11c2318a75
GH <chelsey.gahyun@gmail.com>
2024-08-21 16:11:55 +0900

    re push

--- hyunphoto/users/views.py
+++ hyunphoto/users/views.py
@@ -8,14 +8,33 @@ from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from allauth.socialaccount.models import SocialAccount
 from rest_framework import status
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
 
 class UserSigninview(APIView):
+    
     def get(self, request):
-        return render(request, 'home.html')
+        print('session:', request.session.state)
+        flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+            '../secrets.json',
+            scopes=['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email']
+            )
+        flow.redirect_uri = 'http://127.0.0.1:8000/accounts/google/login/callback'
+
+        authorization_url, state = flow.authorization_url(
+            access_type='offline',
+            include_granted_scopes=True,  # 이미 승인된 권한은 재사용
+            prompt='none'  # 사용자가 이미 인증된 경우 권한 승인 화면을 생략
+        )
+        request.session['state'] = state 
+
+        # return render(request, 'home.html')
+        return redirect(authorization_url)
 
 usersigninview = UserSigninview.as_view()
 
 class UserLogoutView(APIView):
+    permission_classes = [IsAuthenticated]
     def get(self, request):
         logout(request)
         return redirect('/')
@@ -23,12 +42,11 @@ class UserLogoutView(APIView):
 userlogoutview = UserLogoutView.as_view()
 
 class UserProfileView(APIView):
-    permission_classes = [AllowAny]
-    # authentication_classes = []
+    permission_classes = [IsAuthenticated]
 
     def get(self, request):
+        print(request)
         social_account = SocialAccount.objects.filter(user=request.user, provider='google').first()
-        # print(social_account.__dict__)
 
         if social_account:
             user_info = {
