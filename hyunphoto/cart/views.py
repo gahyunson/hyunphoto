c0081e84d53f25a8c84618bc4a0c273c863fc745
GH <chelsey.gahyun@gmail.com>
2024-08-23 23:43:40 +0900

    create cart_item delete api

--- hyunphoto/cart/views.py
+++ hyunphoto/cart/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
@@ -34,5 +35,21 @@ class CartView(APIView):
             'total': total,
         }
         return render(request, 'cart.html', context)
+    
+    def post(self, request, *args, **kwargs):
+        action_type = request.POST.get('action_type')
+        cart_item_id = request.POST.get('cart_item')
+
+        if action_type == 'delete':
+            cart_item = get_object_or_404(Cart, id=cart_item_id, user=request.user)
+            cart_item.delete()
+            return redirect('cart')
+        
+        # elif action_type == 'modify':
+        #     new_quantity = request.POST.get('quantity', 1)
+        #     Cart.objects.filter(id=cart_item_id, user=request.user).update(quantity=new_quantity)
+        #     return redirect('cart')
+        
+        return Response(status=status.HTTP_400_BAD_REQUEST)
     
 cartview = CartView.as_view()
